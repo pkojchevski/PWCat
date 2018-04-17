00cba3d1617cbccccc9513b0d1543c47076582f6
pkojchevski <petarkojchevski@yahoo.com>
2018-04-17 21:53:49 +0200

    initial release

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+
+import {MatToolbarModule, MatCardModule} from '@angular/material';
+import {ImgCardComponent} from './img-card/img-card.component';
+
+@NgModule({
+  declarations: [AppComponent, ImgCardComponent],
+  imports: [BrowserModule, MatToolbarModule, MatCardModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
